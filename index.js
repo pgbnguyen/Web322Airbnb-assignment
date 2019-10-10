663e2a9555e652ad6ea9686384af6b4de3da0c1d
BN <34012277+pgbnguyen@users.noreply.github.com>
2019-10-10 19:53:15 -0400

    Airbnb version: 1.0.0 - Creating files structure and set up dependencies, hosting

---
+++ index.js
@@ -0,0 +1,57 @@
+/*
+//import dependencies to usable objects
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyparser = require('body-parser');
+
+//Creating app express application from express function object 
+const app = express();
+//This load all the static asssets in the public folder, such include css file, images, js file(s)
+app.use(express.static('public'));
+//Set views engine 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// This tells Express to parse all submitted form data into the body of the request object
+app.use(bodyparser.urlencoded({ extended: false }))
+*/
+const express= require("express");
+const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+
+//Create Express APp Ojbect
+const app = express();
+
+
+
+//This load all the static asssets in the public folder, such include css file, images, js file(s)
+app.use(express.static('public'))
+
+
+//This tells Express that I want to use handlebars as my TEMPLATING ENGINE!!!!!!!!!!
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+
+// This tells Express to parse all submitted form data into the body of the request object
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/registration", (red, res)=>{
+    res.render("registration");
+});
+
+app.get("/room-list", (req, res)=>{
+    res.render("room-list");
+});
+PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log(`Connected Successfully to PORT: ${PORT}`);
+});
